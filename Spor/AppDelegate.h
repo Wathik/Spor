1269d6cfc8615237c94936cd382459f1af5180a3
Wathik Almayali <wathikalmayali@Wathik-Almayali.local>
2014-07-05 12:48:19 -0400

    Initial Commit

---
+++ Spor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spor
+//
+//  Created by Wathik Almayali on 7/5/14.
+//  Copyright (c) 2014 Wathik Almayali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
